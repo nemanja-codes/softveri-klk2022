1671adc7efe8e629903b1c081c63bb0a68218311
Nemanja Markovic <necamark@gmail.com>
2024-01-14 02:28:20 +0100

    Napravljene domenske klase i veza sa bazom

---
+++ src/model/Nastavnik.java
@@ -0,0 +1,116 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.Date;
+import java.util.Objects;
+
+/**
+ *
+ * @author necam
+ */
+public class Nastavnik {
+    private int id;
+    private String ime;
+    private String prezime;
+    private Date datumOd;
+    private Date datumDo;
+    private Zvanje zvanje;
+
+    public Nastavnik() {
+    }
+
+    public Nastavnik(int id, String ime, String prezime, Date datumOd, Date datumDo, Zvanje zvanje) {
+        this.id = id;
+        this.ime = ime;
+        this.prezime = prezime;
+        this.datumOd = datumOd;
+        this.datumDo = datumDo;
+        this.zvanje = zvanje;
+    }
+
+    public Date getDatumOd() {
+        return datumOd;
+    }
+
+    public void setDatumOd(Date datumOd) {
+        this.datumOd = datumOd;
+    }
+
+    public Date getDatumDo() {
+        return datumDo;
+    }
+
+    public void setDatumDo(Date datumDo) {
+        this.datumDo = datumDo;
+    }
+
+    
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public String getPrezime() {
+        return prezime;
+    }
+
+    public void setPrezime(String prezime) {
+        this.prezime = prezime;
+    }
+
+    public Zvanje getZvanje() {
+        return zvanje;
+    }
+
+    public void setZvanje(Zvanje zvanje) {
+        this.zvanje = zvanje;
+    }
+
+    @Override
+    public String toString() {
+        return "Nastavnik{" + "id=" + id + ", ime=" + ime + ", prezime=" + prezime + ", zvanje=" + zvanje + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Nastavnik other = (Nastavnik) obj;
+        return this.id == other.id;
+    }
+
+
+    
+    
+    
+    
+    
+}
---
+++ src/model/Zvanje.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author necam
+ */
+public class Zvanje {
+    private int id;
+    private String naziv;
+
+    public Zvanje() {
+    }
+
+    public Zvanje(int id, String naziv) {
+        this.id = id;
+        this.naziv = naziv;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    @Override
+    public String toString() {
+        return naziv;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Zvanje other = (Zvanje) obj;
+        return Objects.equals(this.naziv, other.naziv);
+    }
+    
+    
+    
+    
+}
